574d099984a7925d1d9ac2becf637788c37857ef
James Gater <jgater@gmail.com>
2012-03-23 18:02:22 +0000

    tidy up view when showing individual tickets

--- public/js/manage-viewmodel.js
+++ public/js/manage-viewmodel.js
@@ -6,13 +6,18 @@ function TicketViewModel() {
     self.tabs = ['Open', 'Pending', 'Longterm', 'Closed'];
     self.chosenTabId = ko.observable();
     self.chosenTabData = ko.observableArray();
+    self.chosenTicketData = ko.observable();
 
     // Operations
     self.goToTab = function(tab) { 
-    	self.chosenTabId(tab); 
-    	$.getJSON('api/tickets/status/'+tab, function(allData) {
-        self.chosenTabData(allData);
-    	});
+    	self.chosenTabId(tab);
+    	self.chosenTicketData(null); //stop showing a ticket
+    	$.getJSON('api/tickets/status/'+tab, self.chosenTabData);
+    };
+
+    self.goToTicket = function(ticket) { 
+    	self.chosenTabData(null); // Stop showing a folder
+    	$.getJSON('api/tickets/'+ticket._id, self.chosenTicketData);
     };
     // Start state
     self.goToTab('Open');
