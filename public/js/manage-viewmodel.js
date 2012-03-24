64b8543d44210b5f68e91384741d1905dd000c4a
James Gater <jgater@gmail.com>
2012-03-24 11:13:19 +0000

    lay groundwork for ticket summary data to be calculated client side; show message if no tickets found for a given status.

--- public/js/manage-viewmodel.js
+++ public/js/manage-viewmodel.js
@@ -1,10 +1,17 @@
 //data model
+function TicketSummary(data) {
+    this.age = data.age;
+    this.subject = data.subject;
+    this.from = data.from;
+    this._id = data._id;
+}
 
+// view model
 function TicketViewModel() {
-	// Data
-	var self = this;
+	// associated Data
+	var self = this; //using self avoids scope problems with methods
 	self.tabs = ['Open', 'Pending', 'Longterm', 'Closed'];
-	self.chosenTabId = ko.observable();
+	self.chosenTabId = ko.observable(); // remember, 'observables' are wrapper functions, not actual data structures per se.
 	self.chosenTabData = ko.observable();
 	self.chosenTicketData = ko.observable();
 	self.chosenTicketData.tickets = ko.observableArray();
@@ -15,21 +22,25 @@ function TicketViewModel() {
 	self.goToTicket = function(ticket) {location.hash = self.chosenTabId() + '/' + ticket._id };
 
 	Sammy(function() {
-				this.get('#:tab', function() {
-						self.chosenTicketData(null); //don't show ticket data framework
-						self.chosenTabData(null); // clear tab data first, just in case
-						self.chosenTabId(this.params.tab); //make the selected tab match the request
-						$.getJSON('/api/tickets/status/'+this.params.tab, function(data){
-							self.chosenTabData({tickets: data}); //get all tickets matching status of 'tab', put into chosenTabData.tickets
-						});
+		// show tab view contents, i.e. table
+		this.get('#:tab', function() {
+				self.chosenTicketData(null); //don't show ticket data framework
+				self.chosenTabData(null); // clear tab data first, just in case
+				self.chosenTabId(this.params.tab); //make the selected tab match the request
+				$.getJSON('/api/tickets/status/'+this.params.tab, function(allData){//get all tickets with status of 'tab'
+					var mappedTickets = $.map(allData, function(item) { return new TicketSummary(item) }); //return an array of processed(mapped) tickets
+					self.chosenTabData({tickets: mappedTickets}); //put into chosenTabData.tickets (syntax like this because it's a function...)
 				});
-				this.get('#:tab/:ticketId', function() {
-					self.chosenTabData(null); // Stop showing tab datatable
-					self.chosenTicketData(null); // clear the ticket data first, just in case 
-					self.chosenTabId(this.params.tab); //make the selected tab match the request
-					$.getJSON('/api/tickets/'+this.params.ticketId, self.chosenTicketData); //get ticket details matching requested ticketId, put into chosenTicketData
-				});
-				this.get('', function() { this.app.runRoute('get', '#'+self.tabs[0]) }); //if no specific page requested, open the first tab view
+		});
+		// show individual ticket view
+		this.get('#:tab/:ticketId', function() {
+			self.chosenTabData(null); // Stop showing tab datatable
+			self.chosenTicketData(null); // clear the ticket data first, just in case 
+			self.chosenTabId(this.params.tab); //make the selected tab match the request
+			$.getJSON('/api/tickets/'+this.params.ticketId, self.chosenTicketData); //get ticket details matching requested ticketId, put into chosenTicketData
+		});
+		//default view
+		this.get('/manage', function() { this.app.runRoute('get', '#'+self.tabs[0]) }); //if no specific page requested, open the first tab view
 	}).run();
 };
 
