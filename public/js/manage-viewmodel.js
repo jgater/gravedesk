206e723f10e0b0102fd169475d77d149ce9b4607
James Gater <jgater@gmail.com>
2012-03-24 10:04:58 +0000

    enable client-side routing so deeplinking/bookmarks/back-forward works

--- public/js/manage-viewmodel.js
+++ public/js/manage-viewmodel.js
@@ -9,19 +9,27 @@ function TicketViewModel() {
 	self.chosenTicketData = ko.observable();
 	self.chosenTicketData.tickets = ko.observableArray();
 	// Operations
-	self.goToTab = function(tab) { 
-		self.chosenTicketData(null); //stop showing a ticket
-		self.chosenTabId(tab);
-		$.getJSON('api/tickets/status/'+tab, function(data){
-			self.chosenTabData({tickets: data});
-		});
-	};
-	self.goToTicket = function(ticket) { 
-		self.chosenTabData(null); // Stop showing a folder
-		$.getJSON('api/tickets/'+ticket._id, self.chosenTicketData);
-	};
-	// Start state
-	self.goToTab('Open');
+	
+	// Client-side routing to allow for deeplinking/bookmarks - use sammy library to handle routing results
+	self.goToTab = function(tab) { location.hash = tab };
+	self.goToTicket = function(ticket) {location.hash = self.chosenTabId() + '/' + ticket._id };
+
+	Sammy(function() {
+				this.get('#:tab', function() {
+						self.chosenTicketData(null); //don't show ticket data framework
+						self.chosenTabId(this.params.tab); //make the selected tab match the request
+						$.getJSON('/api/tickets/status/'+this.params.tab, function(data){
+							self.chosenTabData({tickets: data}); //get all tickets matching status of 'tab', put into chosenTabData.tickets
+						});
+				});
+				this.get('#:tab/:ticketId', function() {
+					self.chosenTabData(null); // Stop showing tab datatable
+					self.chosenTicketData(null); // clear the ticket data first, just in case 
+					self.chosenTabId(this.params.tab); //make the selected tab match the request
+					$.getJSON('/api/tickets/'+this.params.ticketId, self.chosenTicketData); //get ticket details matching requested ticketId, put into chosenTicketData
+				});
+				this.get('', function() { this.app.runRoute('get', '#'+self.tabs[0]) }); //if no specific page requested, open the first tab view
+	}).run();
 };
 
 
