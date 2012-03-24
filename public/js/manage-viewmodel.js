543a69c2e354df97c2182fce0b63c723e3f2ad72
James Gater <jgater@gmail.com>
2012-03-24 12:31:32 +0000

    simplify dbhandler, move age calculation to client side - lay the groundwork for further client-side calculation/modification of ticket data before writing back.

--- public/js/manage-viewmodel.js
+++ public/js/manage-viewmodel.js
@@ -1,9 +1,26 @@
 //data model
 function TicketSummary(data) {
-    this.age = data.age;
-    this.subject = data.subject;
-    this.from = data.from;
-    this._id = data._id;
+	this.age = moment(data.date).fromNow(true);
+	this.friendlydate = moment(data.date).format('ddd MMM Do YYYY, hh:mm');
+	this.subject = data.subject;
+	this.from = data.from;
+	this._id = data._id;
+}
+
+function Ticket(data) {
+	this.age = moment(data.date).fromNow();
+	this.friendlydate = moment(data.date).format('ddd MMM Do YYYY, hh:mm');
+	this.subject = data.subject;
+	this.from = data.from;
+	this._id = data._id;
+	this.status = data.status;
+	this.description = data.description;
+	this.notes = data.notes;
+	this.attachments = data.attachments;
+	this.history = data.emails;
+	this.impact = data.impact || "normal";
+	this.cc = data.cc;
+	this.labels = data.labels;
 }
 
 // view model
@@ -19,25 +36,28 @@ function TicketViewModel() {
 	
 	// Client-side routing to allow for deeplinking/bookmarks - use sammy library to handle routing results
 	self.goToTab = function(tab) { location.hash = tab };
-	self.goToTicket = function(ticket) {location.hash = self.chosenTabId() + '/' + ticket._id };
+	self.goToTicket = function(ticket) {location.hash = '/' + ticket._id };
 
 	Sammy(function() {
 		// show tab view contents, i.e. table
 		this.get('#:tab', function() {
-				self.chosenTicketData(null); //don't show ticket data framework
-				self.chosenTabData(null); // clear tab data first, just in case
-				self.chosenTabId(this.params.tab); //make the selected tab match the request
-				$.getJSON('/api/tickets/status/'+this.params.tab, function(allData){//get all tickets with status of 'tab'
-					var mappedTickets = $.map(allData, function(item) { return new TicketSummary(item) }); //return an array of processed(mapped) tickets
-					self.chosenTabData({tickets: mappedTickets}); //put into chosenTabData.tickets (syntax like this because it's a function...)
-				});
+			self.chosenTicketData(null); //don't show ticket data framework
+			self.chosenTabData(null); // clear tab data first, just in case
+			self.chosenTabId(this.params.tab); //make the selected tab match the request
+			$.getJSON('/api/tickets/status/'+this.params.tab, function(allData){ //get all tickets with status of 'tab'
+				var mappedTickets = $.map(allData, function(item) { return new TicketSummary(item) }); //return an array of processed(mapped) tickets
+				self.chosenTabData({tickets: mappedTickets}); //put into chosenTabData.tickets (syntax like this because it's a function...)
+			});
 		});
 		// show individual ticket view
-		this.get('#:tab/:ticketId', function() {
+		this.get('#/:ticketId', function() {
 			self.chosenTabData(null); // Stop showing tab datatable
 			self.chosenTicketData(null); // clear the ticket data first, just in case 
-			self.chosenTabId(this.params.tab); //make the selected tab match the request
-			$.getJSON('/api/tickets/'+this.params.ticketId, self.chosenTicketData); //get ticket details matching requested ticketId, put into chosenTicketData
+			$.getJSON('/api/tickets/'+this.params.ticketId, function(allData){ //get ticket with _id of ticketId
+				var mappedTicket = new Ticket(allData); //return a processed ticket
+				self.chosenTicketData(mappedTicket); //put into chosenTicketData
+				self.chosenTabId(mappedTicket.status || null); //change the active tab to match the current ticket status, or null if not available
+			});
 		});
 		//default view
 		this.get('/manage', function() { this.app.runRoute('get', '#'+self.tabs[0]) }); //if no specific page requested, open the first tab view
