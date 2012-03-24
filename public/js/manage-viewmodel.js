6e5804eb94fb6e237021e6eff96c200449fe99ed
James Gater <jgater@gmail.com>
2012-03-24 09:07:49 +0000

    better hiding of unused elements of view when switching from tab view to ticket view

--- public/js/manage-viewmodel.js
+++ public/js/manage-viewmodel.js
@@ -1,26 +1,27 @@
 //data model
 
 function TicketViewModel() {
-    // Data
-    var self = this;
-    self.tabs = ['Open', 'Pending', 'Longterm', 'Closed'];
-    self.chosenTabId = ko.observable();
-    self.chosenTabData = ko.observableArray();
-    self.chosenTicketData = ko.observable();
-
-    // Operations
-    self.goToTab = function(tab) { 
-    	self.chosenTabId(tab);
-    	self.chosenTicketData(null); //stop showing a ticket
-    	$.getJSON('api/tickets/status/'+tab, self.chosenTabData);
-    };
-
-    self.goToTicket = function(ticket) { 
-    	self.chosenTabData(null); // Stop showing a folder
-    	$.getJSON('api/tickets/'+ticket._id, self.chosenTicketData);
-    };
-    // Start state
-    self.goToTab('Open');
+	// Data
+	var self = this;
+	self.tabs = ['Open', 'Pending', 'Longterm', 'Closed'];
+	self.chosenTabId = ko.observable();
+	self.chosenTabData = ko.observable();
+	self.chosenTicketData = ko.observable();
+	self.chosenTicketData.tickets = ko.observableArray();
+	// Operations
+	self.goToTab = function(tab) { 
+		self.chosenTicketData(null); //stop showing a ticket
+		self.chosenTabId(tab);
+		$.getJSON('api/tickets/status/'+tab, function(data){
+			self.chosenTabData({tickets: data});
+		});
+	};
+	self.goToTicket = function(ticket) { 
+		self.chosenTabData(null); // Stop showing a folder
+		$.getJSON('api/tickets/'+ticket._id, self.chosenTicketData);
+	};
+	// Start state
+	self.goToTab('Open');
 };
 
 
