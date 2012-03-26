511ace9d7713c8c18ef7abeabc2d8695736a8884
James Gater <jgater@gmail.com>
2012-03-26 08:36:36 +0100

    tidy up index view model (list of tickets)

--- public/js/manage-index-viewmodel.js
+++ public/js/manage-index-viewmodel.js
@@ -1,12 +1,3 @@
-//data model
-function TicketSummary(rawticket) {
-	this.id = rawticket._id;
-	this.age = ko.observable( moment(rawticket.date).fromNow(true) );
-	this.friendlydate = ko.observable( moment(rawticket.date).format('ddd MMM Do YYYY, HH:mm') );
-	this.subject = rawticket.subject;
-	this.from = rawticket.from;
-	this.date = rawticket.date;
-}
 
 // view model
 function TabViewModel() {
@@ -22,6 +13,14 @@ function TabViewModel() {
 		});
 	};
 
+	self.getData = function (status) {
+					$.getJSON('/api/tickets/status/'+status, function(allData){ //get all tickets by status
+				var mappedTickets = $.map(allData, function(item) { return new TicketSummary(item) } ); //return an array of processed(mapped) tickets
+				self.tickets(mappedTickets); //put into tickets array (syntax like this because it's a function...)
+				self.sortByDate();
+			});
+				};
+
 	// Client-side routing to allow for deeplinking/bookmarks - use sammy library to handle routing results
 	self.goToTab = function(tab) { location.hash = tab };
 
@@ -29,17 +28,23 @@ function TabViewModel() {
 		// show tab view contents, i.e. table
 		this.get('#:tab', function() {
 			self.chosenTabId(this.params.tab); //make the selected tab match the request
-			$.getJSON('/api/tickets/status/'+this.params.tab, function(allData){ //get all tickets with status of 'tab'
-				var mappedTickets = $.map(allData, function(item) { return new TicketSummary(item) } ); //return an array of processed(mapped) tickets
-				self.tickets(mappedTickets); //put into tickets array (syntax like this because it's a function...)
-				self.sortByDate();
-			});
+			self.getData(this.params.tab);
 		});
 
 		//default view
 		this.get('/manage', function() { this.app.runRoute('get', '#'+self.tabs[0]) }); //if no specific page requested, open the first tab view
 	}).run();
 };
+
+//data model
+function TicketSummary(rawticket) {
+	this.id = rawticket._id;
+	this.age = ko.observable( moment(rawticket.date).fromNow(true) );
+	this.friendlydate = ko.observable( moment(rawticket.date).format('ddd MMM Do YYYY, HH:mm') );
+	this.subject = rawticket.subject;
+	this.from = rawticket.from;
+	this.date = rawticket.date;
+}
 
 //get to work!
 ko.applyBindings(new TabViewModel());
