432d72b40214edb642d7733316e4ca97645e598f
James Gater <jgater@gmail.com>
2012-04-18 16:30:25 +0100

    simply top bar view model

--- public/js/topbar-viewmodel.js
+++ public/js/topbar-viewmodel.js
@@ -2,17 +2,10 @@
 function TopBarViewModel() {
 	// associated Data
 	var self = this; //using self avoids scope problems with methods
-	self.navbarLinks = [{"path":"/", "name": "Home", "icon":"icon-home", "showOnlyToAdmin": false}
-	, {"path":"/manage", "name":"Manage Tickets", "icon":"icon-th-list", "showOnlyToAdmin": false}
-	, {"path":"/admin", "name":"Admin Site", "icon":"icon-cog", "showOnlyToAdmin": true}
-	, {"path":"/account", "name":" My Account", "icon":"icon-user", "showOnlyToAdmin": false}
-	];
-
 	self.matchingPath = function (linkpath) {
 		var locationpath = location.pathname.substring(0,location.href.indexOf("/")+1);
 		return (linkpath == location.pathname || linkpath == locationpath);
 	};
-
 };
 
 ko.bindingHandlers['class'] = {
